dc02f0d326863ad3bc7ddb431688b35771d2b4af
Justin <jlsenterfitt@gmail.com>
2018-12-29 16:20:43 +0000

    Start outline of optimizer.

--- optimizer/optimizer.py
+++ optimizer/optimizer.py
@@ -1,23 +1,27 @@
 """Optimize allocations for a given date.
 
 General strategy is:
-    1) Filter given prices data based on date.
-        1.1) Remove tickers w/o enough data.
-        1.2) Remove days w/o all tickers.
-    2) Fork several worker processes with copies of the filtered data.
-    3) From some arbitrary starting point, create a queue of all potential
+    1) Fork several worker processes with copies of the filtered data.
+    2) From some arbitrary starting point, create a queue of all potential
         single trade deviations.
-    4) Have workers process the queue, and send results back via queue.
-    5) Have the master process the results, choosing the best one.
-    6a) If a better allocation is found, restart #3 with that allocation as the
+    3) Have workers process the queue, and send results back via queue.
+    4) Have the master process the results, choosing the best one.
+    5a) If a better allocation is found, restart #3 with that allocation as the
         starting point.
-    6b) If no better allocation is found, halve the trading amount and restart
+    5b) If no better allocation is found, halve the trading amount and restart
         #2 with the current allocation and new trade amount.
-    7) Once a lower limit of trade amount is found, return the allocaiton.
+    6) Once a lower limit of trade amount is found, return the allocaiton.
 """
+import multiprocessing as mp
+import numpy as np
+
+def _initializeProcess(data):
+    """Initialize a process with necessary data."""
+    global data_matrix
+    data_matrix = data
 
 
-def _scoreAllocation(data_matrix, allocation_array, required_return):
+def _scoreAllocation(allocation_array, required_return):
     """Determine the score of a given allocation.
 
     Args:
@@ -30,7 +34,7 @@ def _scoreAllocation(data_matrix, allocation_array, required_return):
     pass
 
 
-def _findOptimalAllocation(data_matrix, ticker_tuple, required_return):
+def findOptimalAllocation(data_matrix, ticker_tuple, required_return):
     """Find the optimal allocation.
 
     Args:
@@ -41,16 +45,18 @@ def _findOptimalAllocation(data_matrix, ticker_tuple, required_return):
     Returns:
         allocations: Dict of percent allocations by ticker.
     """
-    pass
+    _initializeProcess(data_matrix)
 
+    with mp.Pool(
+            initializer=_initializeProcess,
+            initArgs=(data_matrix,)) as pool:
 
-def getOptimalAllocation(data_matrix, required_return):
-    """Find the optimal allocation based on requirements.   
+        best = np.zeros(len(ticker_tuple), dtype=float64)
+        best[0] = 1.0
+        best_score = _scoreAllocation(ticker_tuple, required_return)
 
-    Args:
-        data_matrix: See _convertTickerDataToMatrix.
-        required_return: What daily return is desired.
-    Returns:
-        allocations: Dict of percent allocations by ticker.
-    """
-    pass
+        # Create deviations from starting point, filter them, send through queues.
+        # Collect results, keep best.
+        # If no improvement, halve trade amount.
+        # Repeat until trade amount is exhausted.
+    
